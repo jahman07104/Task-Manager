7f5cc09460b6fb2cbefa3b177ab99ecf72518edb
Patrick Harrison <jahman07104@gmail.com>
2020-01-30 15:24:03 -0500

    tickets can be CRUDed

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,6 +1,51 @@
 class TicketsController < ApplicationController
-  def index
+  before_action :find_ticket, only: [:edit, :update, :destroy, :show]
 
-    
+  def index
+    @tickets = Ticket.all
+  end
+
+  def show
+  end 
+  
+  def new
+    @ticket = Ticket.new
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+    @ticket.user = current_user
+
+    if @ticket.save
+      redirect_to tickets_path, notice: "ticket succesfully created"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @ticket.update(ticket_params)
+      redirect_to tickets_path, notice: "ticket succesfully updated"
+    else
+      render :edit
+    end  
+  end
+
+  def destroy
+    @ticket.destroy
+    redirect_to tickets_path, notice: "ticket succesfully deleted"
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:customer_name, :customer_address, :customer_phone_number, :customer_details)
+  end
+
+  def find_ticket
+    @ticket = Ticket.find_by(id: params[:id])
   end
 end
